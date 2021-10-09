014f08213b15546482effc55ccfb0da88e9bf0cd
sergey <kurilovsergey15@gmail.com>
2021-10-09 16:16:45 +0300

    61

---
+++ src/components/spinner/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-joyhxbd3vc">
+            <div className="ldio-ubjprwr717e">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Spinner;
---
+++ src/components/starship-details/starship-details.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class StarshipDetails extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default StarshipDetails;
